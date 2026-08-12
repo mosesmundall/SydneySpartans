31924be3832b884dd93b6f22f02de91945efdd89
mosesmundall <fishingiscool4me@gmail.com>
2026-08-12 14:14:22 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const CONFIG = {
       dimi_b: "/dimi_champ.png",
       jacob_s: "/shiggs_champ.png",
       aidan_b: "/aidan_champ.png",
-      harrison_r: "/harrison_r.png",
+      harrison_r: "/harrison_champ.png",
       
     },
     size: 72,
